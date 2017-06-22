54e826e28f20ab25dcc12e2204a813e8c424a095
Fabien Ryckoort <fabien.ryckoort@7digital.com>
2017-06-22 09:49:36 +0200

    .

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var config = require('./config');
-var defaultSchema = require('./assets/7digital-api-schema');
-var _ = require('lodash');
+// var defaultSchema = require('./assets/7digital-api-schema');
+// var _ = require('lodash');
 
-module.exports = require('./lib/api').Api.buildDefault();
+// module.exports = require('./lib/api').Api.buildDefault();
 
-module.exports.configure = function (options, schema) {
-	options = options || {};
-	schema = schema || defaultSchema;
+// module.exports.configure = function (options, schema) {
+// 	options = options || {};
+// 	schema = schema || defaultSchema;
 
-	_.defaults(options, config);
+// 	_.defaults(options, config);
 
-	return require('./lib/api').Api.build(options, schema);
-};
+// 	return require('./lib/api').Api.build(options, schema);
+// };
 
-module.exports.schema = require('./assets/7digital-api-schema');
+// module.exports.schema = require('./assets/7digital-api-schema');
